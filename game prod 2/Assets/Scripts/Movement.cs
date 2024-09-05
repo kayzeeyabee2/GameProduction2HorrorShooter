ee0bcfdf5514354d18995a5b2ed0f47a04057373
kayzeeyabee2 <kgosikubheka291@gmail.com>
2024-09-05 10:14:47 +0200

    Created repository and added unity file

---
+++ game prod 2/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundPlayer;
+    private float playerSpeed = 5f;
+    private float gravityValue = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       controller = gameObject.GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (groundPlayer && playerVelocity.y <= 0)
+        {
+            playerVelocity.y = 0;
+        }
+        //Rotates player in the direction of movement
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        
+    }
+}
